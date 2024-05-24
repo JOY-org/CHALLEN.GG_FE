97bef1064fb7e1a8a6d188057efaa67390a2c76f
KIMJINYOUNG <k5444862@naver.com>
2024-05-25 02:45:40 +0900

    게시글 작성자 팔로우

--- src/components/CommunityPostModal.jsx
+++ src/components/CommunityPostModal.jsx
@@ -7,81 +7,187 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import ModeCommentIcon from '@mui/icons-material/ModeComment';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useState } from "react";
+import ModeCommentIcon from "@mui/icons-material/ModeComment";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Button } from "@mui/material";
-import { postApi } from "../api/services/post";
 import { useAuth } from "../hooks/useAuth";
 import axios from "axios";
-import { useCallback } from "react";
 import Swal from "sweetalert2";
+import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
 
+export default function CommunityPostModal({
+  postDetail,
+  setPosts,
+  posts,
+  handleClosePost,
+}) {
+  const { loginUser } = useAuth();
+  const deletePost = async (id) => {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_API_URL}/post/${id}`,
+      {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      }
+    );
+    if (res.data.code === 200) {
+      setPosts(posts.filter((p) => p.id !== id));
+      Swal.fire({
+        text: res.data.message,
+        icon: "success",
+      });
+      handleClosePost();
+    }
+  };
 
+  const likePost = async (id) => {
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/post/postlike`,
+        {
+          id: id,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      if (res.data.code === 200) {
+        Swal.fire({
+          text: res.data.message,
+          icon: "success",
+        });
+      } else {
+        Swal.fire({
+          text: res.data.message,
+          icon: "error",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        text: "게시물에 좋아요를 누르는 중 오류가 발생했습니다.",
+        icon: "error",
+      });
+    } finally {
+      handleClosePost();
+    }
+  };
 
-export default function CommunityPostModal({postDetail, setPosts, posts, handleClosePost}) {
-  const {loginUser} = useAuth()
-
+  // 게시글 작성자 팔로우 관련
   const [myFollowing, setMyFollowing] = useState();
 
-    const deletePost = async (id) => {
-      const res = await axios.delete(`${process.env.REACT_APP_API_URL}/post/${id}`, {
-          headers: {
-              "Authorization": localStorage.getItem("token"),
-          },
-      });
-      if (res.data.code === 200) {
-          setPosts(posts.filter(p => p.id !== id));
-          Swal.fire({
-              text: res.data.message,
-              icon: "success"
-          })
-          handleClosePost()
-      }
+  const getFollowings = async()=>{
+    try {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/users/followings/${loginUser?.id}`,{
+            headers: {
+                Authorization: loginUser.token
+            }
+        })
+        setMyFollowing(res.data.payload);
+        // console.log(res.data.payload);
+    } catch (error){
+        console.error(error);
+    }
   }
+  useEffect(()=>{
+    getFollowings();
+  }, []);
 
+  const followUser = async (id) => {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/users/follow`,
+      {
+        id,
+      },
+      {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      }
+    );
+    if (res.data.code === 200) {
+      Swal.fire({
+        text: res.data.message,
+        icon: "success",
+      });
+      getFollowings()
+      handleClosePost();
+    }
+  };
 
-  
+  const unfollowUser = async (id) => {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_API_URL}/users/follow`,
+      {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        data: {
+          id,
+        },
+      }
+    );
+    if (res.data.code === 200) {
+      Swal.fire({
+        text: res.data.message,
+        icon: "success",
+      });
+      getFollowings()
+      handleClosePost();
+    }
+  };
 
   return (
-      <Card sx={{ boxShadow: 'none'}}>
-        <CardHeader
-          avatar={
-            <Avatar
-              src='https://images.unsplash.com/photo-1551884831-bbf3cdc6469e?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            />
-          }
+    <Card sx={{ boxShadow: "none" }}>
+      <CardHeader
+        avatar={<Avatar src={`http://localhost:8000/${postDetail.User.img}`} />}
+        title={postDetail.User.nickname}
+        subheader={postDetail.createdAt.slice(0, 10)}
+        action={ (loginUser.id && loginUser.id !== postDetail.UserId) ?
+            (myFollowing?.findIndex(f => f.id === postDetail.UserId) !== -1 ?
+              <Button onClick={() => unfollowUser(postDetail.UserId)}>팔로우 취소</Button>
+              :
+              <Button onClick={() => followUser(postDetail.UserId)}>팔로우</Button>
+            )
+          :
+            ''
+        }
+      />
 
-          title={postDetail.User.nickname}
-          subheader={postDetail.createdAt.slice(0,10)}
-        />
-        <CardMedia
-          component="img"
-          height="250"
-          image={`http://localhost:8000/${postDetail.img}`}
-          alt="본문 이미지"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {postDetail.content}
-          </Typography>
-        </CardContent>
-        <CardActions style={{display:'flex', justifyContent:'space-between'}}>
-          <div>
-            <IconButton>
-                <FavoriteBorderIcon style={{color:'red'}}/> 
-                {/* <FavoriteIcon style={{color:'red'}}/> */}
-            </IconButton>
-            <IconButton>
-              <ModeCommentIcon />
-            </IconButton>
-          </div>
-          {loginUser.id === postDetail.UserId &&
-            <Button onClick={()=>deletePost(postDetail.id)}>삭제</Button>
-          }
-          
-        </CardActions>
-      </Card>
+      <CardMedia
+        component="img"
+        height="200"
+        image={`http://localhost:8000/${postDetail.img}`}
+        alt="본문 이미지"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {postDetail.content}
+        </Typography>
+      </CardContent>
+      <CardActions style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <IconButton>
+            <FavoriteBorderIcon
+              style={{ color: "red" }}
+              onClick={() => {
+                likePost(postDetail.id);
+              }}
+            />
+            {/* <FavoriteIcon style={{color:'red'}}/> */}
+          </IconButton>
+          <IconButton>
+            <ModeCommentIcon />
+          </IconButton>
+        </div>
+        {loginUser.id === postDetail.UserId && (
+          <Button onClick={() => deletePost(postDetail.id)}>삭제</Button>
+        )}
+      </CardActions>
+    </Card>
   );
 }
