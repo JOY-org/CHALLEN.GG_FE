4da0749a488b9d084cb8acd88a68211d37bed130
KIMJINYOUNG <k5444862@naver.com>
2024-05-31 15:28:44 +0900

    postcoment 컴포넌트 분리

--- src/pages/community/CommunityPostModal.jsx
+++ src/pages/community/CommunityPostModal.jsx
@@ -10,19 +10,7 @@ import Typography from "@mui/material/Typography";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import {
-  Box,
-  Button,
-  Divider,
-  InputAdornment,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Pagination,
-  Stack,
-  TextField,
-} from "@mui/material";
+import {Button} from "@mui/material";
 import { useAuth } from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import { useState } from "react";
@@ -30,8 +18,7 @@ import { useEffect } from "react";
 import { postApi } from "../../api/services/post";
 import { userApi } from "../../api/services/user";
 import { getRelativeTime } from "../../utils/date";
-import AddCommentIcon from "@mui/icons-material/AddComment";
-import { useForm } from "react-hook-form";
+import PostComment from "./PostComment";
 
 export default function CommunityPostModal({
   postDetail,
@@ -42,20 +29,11 @@ export default function CommunityPostModal({
   setPostComment,
   getComment,
 }) {
-  const SHOW_POST_NUM = 5;
+
   const { loginUser } = useAuth();
-  const [curPage, setCurPage] = useState(1);
-  const [totalPage, setTotalPage] = useState();
   const [pressedLike, setPressedLike] = useState();
   const [myFollowing, setMyFollowing] = useState();
   const [showComment, setShowComment] = useState(false);
-
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { isSubmitting, errors },
-  } = useForm({ mode: "onChange" });
 
   // 게시글 삭제
   const deletePost = async (id) => {
@@ -111,49 +89,6 @@ export default function CommunityPostModal({
   const toggleComment = () => {
     setShowComment((prev) => !prev);
   };
-
-  console.log(postComment);
-  const onRegist = async (data) => {
-    try {
-      const commentInput = {
-        content: data.content,
-        postId: postDetail.id,
-      };
-      const res = await postApi.uploadComment(
-        commentInput,
-        localStorage.getItem("token")
-      );
-      if (res.data.code === 200) {
-        setPostComment(res.data.payload);
-        reset();
-      } else {
-        throw new Error("알 수 없는 에러");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const deleteComment = async (id) => {
-    const res = await postApi.deleteComment(id, localStorage.getItem("token"));
-    if (res.data.code === 200) {
-      setPostComment(postComment.filter((c) => c.id !== id));
-      Swal.fire({
-        text: res.data.message,
-        icon: "success",
-      });
-    }
-  };
-
-  // pagination 관련
-  useEffect(() => {
-    setTotalPage(Math.floor(postComment?.length / SHOW_POST_NUM) + 1);
-  }, [postComment]);
-
-  const handlePage = (e, v) => {
-    setCurPage(v);
-  };
-  // --------
 
   // 게시글 좋아요 관련
   const getLikedPostsByUserId = async (id) => {
@@ -288,90 +223,7 @@ export default function CommunityPostModal({
         )}
       </CardActions>
       <CardActions>
-        {showComment && (
-          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-            <Box
-              component="form"
-              onSubmit={handleSubmit(onRegist)}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                multiline
-                sx={{ width: "100%", marginBottom: "10px" }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton type="submit" disabled={isSubmitting}>
-                        <AddCommentIcon />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                autoComplete="off"
-                {...register("content", {
-                  required: "댓글은 필수 입력입니다.",
-                  maxLength: {
-                    value: 150,
-                    message: "댓글은 최대 150자까지 입력가능합니다.",
-                  },
-                })}
-                spellCheck={false}
-                error={errors.content ? true : false}
-                helperText={errors.content && errors.content.message}
-              />
-            </Box>
-            {/* 댓글목록 뿌려주기 */}
-            {postComment &&
-              postComment
-                .slice(SHOW_POST_NUM * (curPage - 1), SHOW_POST_NUM * curPage)
-                .map((c) => {
-                  return (
-                    <>
-                      <Divider variant="inset" component="li" />
-                      <ListItem alignItems="flex-start">
-                        <ListItemAvatar>
-                          <Avatar src={`http://localhost:8000/${c.User.img}`} />
-                        </ListItemAvatar>
-                        <ListItemText
-                          sx={{ width: "80%" }}
-                          secondary={
-                            <React.Fragment>
-                              <Typography
-                                sx={{ display: "inline" }}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                              >
-                                {c.User.nickname}
-                                <p
-                                  style={{
-                                    marginRight: "0px",
-                                    fontSize: "12px",
-                                  }}
-                                >
-                                  {getRelativeTime(c.createdAt)}
-                                </p>
-                              </Typography>
-                              <br />
-                              {c.content}
-                            </React.Fragment>
-                          }
-                        />
-                        {loginUser === c.UserId && (
-                          <Button onClick={()=>{deleteComment(c.id)}}>
-                            삭제
-                          </Button>
-                        )}
-                      </ListItem>
-                    </>
-                  );
-                })}
-            <Stack sx={{ alignItems: "center", mt: "20px" }}>
-              <Pagination count={totalPage} onChange={handlePage}></Pagination>
-            </Stack>
-          </List>
-        )}
+        {showComment && <PostComment postComment={postComment} setPostComment={setPostComment} postDetail={postDetail}/>}
       </CardActions>
     </Card>
   );
