34986e7f28b33c6c29cf4935fa3cd2f824821b48
jodasom <jodasom0216@gmail.com>
2024-06-04 07:43:07 +0900

    menufiltererror

--- src/pages/homes/components/MenuFilter.jsx
+++ src/pages/homes/components/MenuFilter.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from "../../../hooks/useAuth";
-import Btn from "../../../components/Btn";
+import Btn from "./Btn";
 import styleHome from "../css_module/Home.module.css"
 import TopAd from "./TopAd";
 
@@ -13,7 +13,6 @@ const Menufilter = ({setSortKey,handleSearchInputChange,handleSearch}) => {
 
     return (
         <>
-            {loginUser ?
                 <div className={styleHome.btn}>
                     {/* 그냥 전체 나열 */}
                     <Btn btnEvent={()=>handleFilterClick('전체')}>전체</Btn>
@@ -24,21 +23,12 @@ const Menufilter = ({setSortKey,handleSearchInputChange,handleSearch}) => {
                     {/* max최대인원수가 가장 적게 남은 사람 */}
                     <Btn btnEvent={()=>handleFilterClick('인기')}>인기</Btn>
                     {/* 로그인한 사용자가 좋아요한 리스트 */}
-                    <Btn btnEvent={()=>handleFilterClick('관심')}>관심</Btn>
+                    {loginUser && <Btn btnEvent={() => handleFilterClick('관심')}>관심</Btn>}
                     {/* 제목을 기준으로 */}
                     <input type="text" onChange={handleSearchInputChange} ></input><TopAd/>
                     <Btn btnEvent={handleSearch}>검색</Btn>
                 </div>
-                :
-                <div className={styleHome.btn}>
-                    <Btn btnEvent={()=>handleFilterClick('전체')}>전체</Btn>
-                    <Btn btnEvent={()=>handleFilterClick('신규')}>신규</Btn>
-                    <Btn btnEvent={()=>handleFilterClick('마감임박')}>마감임박</Btn>
-                    <Btn btnEvent={()=>handleFilterClick('인기')}>인기</Btn>
-                    <input type="text"></input><TopAd/>
-                    <Btn>검색</Btn>
-                </div>
-            }
+
         </>
     );
 }
