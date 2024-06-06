476e78852e61d390fc0c0ee709427cbe39df39a8
jodasom <jodasom0216@gmail.com>
2024-06-06 10:02:37 +0900

    AdMessage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from 'react';
 import styles from '../components/css_module/Banner.module.css';
 
-const Message = ({ children, style  }) => {
-
+const Message = ({ children, style }) => {
     return (
         <button style={style} className={`${styles.btn} ${styles['btn-marquee']}`}>
-            <span data-text='안녕하세요 challen.gg 입니다.여러분의 도전을 응원합니다'>{children}</span>
+            <span>{children}</span>
+            <span className={styles['marquee-text']}></span>
         </button>
     );
 }
 
 export default Message;
-
-
-//제이슨으로 리스트 빼기 
